535516f81a5c7fe702ddd97694d7ffb4c31bb4e7
Nyurgun <burtsev.ns@gmail.com>
2022-03-25 17:21:38 +0900

    Add second vc.

---
+++ GCD/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  GCD
+//
+//  Created by Нюргун on 25.03.2022.
+//  Copyright © 2022 Нюргун. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
