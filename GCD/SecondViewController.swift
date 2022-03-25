032e5cc291c9f1e38f33153c01d9f2e1dc78d8ab
Nyurgun <burtsev.ns@gmail.com>
2022-03-25 23:14:56 +0900

    Using Dispatch After

--- GCD/SecondViewController.swift
+++ GCD/SecondViewController.swift
@@ -30,8 +30,40 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchImage()
+        delay(3) {
+            self.loginAlert()
+        }
     }
     
+    //Функция отсроченного выполнения кода
+    fileprivate func delay(_ delay: Int, clousure: @escaping () -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delay)) {
+            clousure()
+        }
+    }
+    
+    //Всплывающее окно
+    fileprivate func loginAlert() {
+        let ac = UIAlertController(title: "Зарегистрированы?", message: "Введите ваш логин и пароль", preferredStyle: .alert)
+        //Добавляем кнопки
+        let okAction = UIAlertAction(title: "Ок", style: .default, handler: nil)
+        let cancelAction = UIAlertAction(title: "Отмена", style: .default, handler: nil)
+        //Добавляем действия в алерт контроллер
+        ac.addAction(okAction)
+        ac.addAction(cancelAction)
+        //Добавляем текстовые поля в алерт контроллер
+        ac.addTextField { (usernameTF) in
+            usernameTF.placeholder = "Введите логин"
+        }
+        ac.addTextField { (userPasswordTF) in
+            userPasswordTF.placeholder = "Введите пароль"
+            userPasswordTF.isSecureTextEntry = true
+        }
+        //Отображаем алерт контроллер
+        self.present(ac, animated: true, completion: nil)
+    }
+    
+    //Загрузка изображения
     fileprivate func fetchImage() {
         imageURL = URL(string: "https://upload.wikimedia.org/wikipedia/commons/0/07/Huge_ball_at_Vilnius_center.jpg")
         activityIndicator.isHidden = false
